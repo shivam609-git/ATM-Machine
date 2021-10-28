e2e3ba6a74f3002a97dbaaa0e12ccff2db50e73c
shivam <sdsharma609@gmail.com>
2021-10-28 19:06:48 +0530

    entry of the project

--- Atm.java
+++ Atm.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Atm extends MainMenu{
     public static void main(String[] args) throws IOException{
         MainMenu mainMenu = new MainMenu();
-
+        //MainEntryPoint
         mainMenu.getLogin();
     }
 }
